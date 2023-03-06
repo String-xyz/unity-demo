713317e1360d198f4f8ae11c5319822eaa304d44
Auroter <7332587+Auroter@users.noreply.github.com>
2023-03-06 13:27:20 -0700

    Wallet (#3)
    * Adding MetaMask integration
    
    * Demo now working with MetaMask
    
    * Fix compile errors with MetaFab update
    
    * update

--- Assets/GameLogicBehavior.cs
+++ Assets/GameLogicBehavior.cs
@@ -6,22 +6,73 @@ using Cysharp.Threading.Tasks;
 using System;
 using System.Linq;
 using StringSDK;
+using MetaMask.Unity;
+using MetaMask.Models;
+using System.Text;
 
 public class GameLogicBehavior : MonoBehaviour
 {
-    // These will be passed into our String package
+    // Used to determine where we derive the wallet data from
+    private bool usingMetaFab;
+
+    // These Metafab vars will be passed into our String package
     private string playerWallet;
     private string playerWalletID;
-    private string playerDecryptKey;
 
     // Storing data from our String package for convenience
     private string stringPlayerID;
     private TransactionRequest lastQuote;
     private TransactionResponse lastTransaction;
 
+    // Metamask vars, not required
+    private MetaMask.MetaMaskWallet metaMaskWallet;
+
     // Start is called before the first frame update
-    async UniTaskVoid Start()
+    void Start()
     {
+        // Initialize the string SDK with our API key
+        StringXYZ.ApiKey = "str.d5f47fb041554135a89038e469d5a762";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnWalletConnected(object sender, EventArgs e)
+    {
+        Debug.Log($"MetaMask Connected to Player Wallet");
+    }
+
+    void OnWalletAuthorized(object sender, EventArgs e)
+    {
+        Debug.Log($"MetaMask Wallet was Authorized by the Player");
+    }
+
+    public void InitializeMetaMask()
+    {
+        usingMetaFab = false;
+
+        // Init singleton client
+        MetaMaskUnity.Instance.Initialize();
+
+        // Get handle to wallet
+        metaMaskWallet = MetaMaskUnity.Instance.Wallet;
+
+        // Connect wallet
+        metaMaskWallet.Connect();
+
+        // Subscribe our handler to wallet connection event
+        metaMaskWallet.WalletConnected += OnWalletConnected;
+
+        // Subscribe our handler to wallet authorization event
+        metaMaskWallet.WalletAuthorized += OnWalletAuthorized;
+    }
+
+    public async void InitializeMetaFab()
+    {
+        usingMetaFab = true;
+
         // Simulate Authing the Game with Metafab
         Debug.Log("Authing game with MetaFab...");
         var response = await Metafab.GamesApi.AuthGame(Config.Email, Config.Password, default);
@@ -29,79 +80,98 @@ public class GameLogicBehavior : MonoBehaviour
 
         // Metafab needs us to copy the response data
         Config.PublishedKey = response.publishedKey;
-		Metafab.PublishedKey = Config.PublishedKey;
-		Metafab.SecretKey = response.secretKey;
-		Metafab.Password = Config.Password;
+        Metafab.PublishedKey = Config.PublishedKey;
+        Metafab.SecretKey = response.secretKey;
+        Metafab.Password = Config.Password;
 
         // Simulate there being a player
         // get player and authenticate
-		Debug.Log($"Getting player...");
-		var players = await Metafab.PlayersApi.GetPlayers();
+        Debug.Log($"Getting player...");
+        var players = await Metafab.PlayersApi.GetPlayers();
         PublicPlayer player;
-        if (players.Count == 0) {
+        if (players.Count == 0)
+        {
             // Create a player if one doesn't exist
             CreatePlayerRequest createRequest = new("sample-player", "password");
-            await Metafab.PlayersApi.CreatePlayer(createRequest);
+            await Metafab.PlayersApi.CreatePlayer(createRequest, 0);
 
             players = await Metafab.PlayersApi.GetPlayers();
             player = players[players.Count - 1];
             Debug.Log($"Created first player: {player}");
-        } else {
+        }
+        else
+        {
             player = players[players.Count - 1];
             Debug.Log($"Got player: {player}");
         }
 
         // Simulate that player logging in with their credentials
-		Debug.Log($"Authing player...");
-		var auth = await Metafab.PlayersApi.AuthPlayer("sample-player", "password");
-		Debug.Log($"Got auth {auth}");
+        Debug.Log($"Authing player...");
+        var auth = await Metafab.PlayersApi.AuthPlayer("sample-player", "password", 0);
+        Debug.Log($"Got auth {auth}");
 
         // Store the information we need about the player to be passed into our String package
         // AuthPlayer response will be updated tomorrow, we can get the decrypt key then.
         playerWallet = auth.wallet.address;
         playerWalletID = auth.wallet.id;
-        playerDecryptKey = auth.walletDecryptKey;
 
-        // Initialize the string SDK with our API key
-        StringXYZ.ApiKey = "str.4efebe2a16e84336b0feec7f9238a663";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Metafab. = auth.walletDecryptKey;
     }
 
     public async void LoginPlayerToString()
     {
-        // Real
+        string walletAddr;
+        if (usingMetaFab)
+        {
+            walletAddr = playerWallet;
+        }
+        else
+        {
+            walletAddr = metaMaskWallet.SelectedAddress;
+        }
 
-        //LoginPayload payloadToSign = await StringXYZ.RequestLogin(playerWallet);
-        //Debug.Log($"Wallet Login Payload: {payloadToSign}");
-
-        //////Metafab.PlayerDecryptKey = playerDecryptKey; // This doesn't work yet.
-        //Metafab.Password = "password"; // Temporary hack to get around SDK missing header
-        //CreateWalletSignatureRequest signatureRequest = new(payloadToSign.nonce);
-        //string signedPayload = await Metafab.WalletsApi.CreateWalletSignature(playerWalletID, signatureRequest);
-
-        //LoginRequest login = new LoginRequest(
-        //    nonce: payloadToSign.nonce,
-        //    signature: signedPayload,
-        //    visitorId: "dle6eqRHxjPEj4H3WLoC",
-        //    requestId: "1671054875232.EcrKjS"
-        //);
-
-        //var response = await StringXYZ.Login(login);
-        //Debug.Log($"Login response = {response}");
-        //StringXYZ.Authorization = response.authToken.token;
-
-        // TESTING
-        LoginPayload payloadToSign = await StringXYZ.RequestLogin("0x44A4b9E2A69d86BA382a511f845CbF2E31286770");
+        LoginPayload payloadToSign = await StringXYZ.RequestLogin(walletAddr);
         Debug.Log($"Wallet Login Payload: {payloadToSign}");
 
+        var base64decode = Encoding.UTF8.GetString(Convert.FromBase64String(payloadToSign.nonce));
+
+
+        string sig = "";
+        if (usingMetaFab)
+        {
+            var request = new CreateWalletSignatureRequest(base64decode);
+            try
+            {
+                var headers = new Dictionary<string, string>
+                {
+                    ["X-Wallet-Decrypt-Key"] = Metafab.WalletDecryptKey
+                };
+                sig = await Metafab.WalletsApi.CreateWalletSignature(playerWalletID, request, default, headers);
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"ERROR = {e}");
+                return;
+            }
+        }
+        else
+        {
+            var parameters = new string[] { walletAddr, base64decode };
+
+            var request = new MetaMaskEthereumRequest
+            {
+                Method = "personal_sign",
+                Parameters = parameters
+            };
+            var res = await metaMaskWallet.Request(request);
+            sig = res.GetString();
+        }
+        Debug.Log($"SIGNATURE RESPONSE = {sig}");
+
+
         LoginRequest login = new LoginRequest(
-            nonce: "VGhhbmsgeW91IGZvciB1c2luZyBTdHJpbmchIEJ5IHNpZ25pbmcgdGhpcyBtZXNzYWdlIHlvdSBhcmU6CgoxKSBBdXRob3JpemluZyBTdHJpbmcgdG8gaW5pdGlhdGUgb2ZmLWNoYWluIHRyYW5zYWN0aW9ucyBvbiB5b3VyIGJlaGFsZiwgaW5jbHVkaW5nIHlvdXIgYmFuayBhY2NvdW50LCBjcmVkaXQgY2FyZCwgb3IgZGViaXQgY2FyZC4KCjIpIENvbmZpcm1pbmcgdGhhdCB0aGlzIHdhbGxldCBpcyBvd25lZCBieSB5b3UuCgpUaGlzIHJlcXVlc3Qgd2lsbCBub3QgdHJpZ2dlciBhbnkgYmxvY2tjaGFpbiB0cmFuc2FjdGlvbiBvciBjb3N0IGFueSBnYXMuCgpOb25jZToga3FCam5MeHlHTk5rVG9GcnU4MWJUNnZwcGttRGdhTnZRSlcyOVhsMXIwZlJZY0tHaHp4M0dxbWNjQjA2eFVuNGlFUUdCbTVDYXBZNmNlUTV1cmM3WXg4amsxbUxGWE1NQ25meUkzdW9ZZTJDVFFEbGNETzFESmRwZEFnMDkrWT0=",
-            signature: "0x573aa11f2833bb03b9847a00551f585583c094ef1189fb7a160b51278a5a9c770f0396ab88fb6424ed6998c83cd4c6f8e5030a902cb2f271107a4fb25b4b843300",
+            nonce: payloadToSign.nonce,
+            signature: sig,
             visitorId: "dle6eqRHxjPEj4H3WLoC",
             requestId: "1671054875232.EcrKjS"
         );
@@ -115,7 +185,7 @@ public class GameLogicBehavior : MonoBehaviour
     {
         QuoteRequest quoteRequest = new QuoteRequest(
             userAddress: "0x44A4b9E2A69d86BA382a511f845CbF2E31286770",
-            chainID: 43113,
+            chainId: 43113,
             contractAddress: "0x861aF9Ed4fEe884e5c49E9CE444359fe3631418B",
             contractFunction: "mintTo(address)",
             contractReturn: "uint256",
